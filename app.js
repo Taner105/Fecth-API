9ee40424e2a5ffb1bbeba4683e20ff501fc6bdd4
Taner105 <taneraydogan59@gmail.com>
2021-11-27 19:32:24 +0300

    .

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function getUserList() {
     .then(data =>{
         // console.log(data);
         for(user of data.data){
-            console.log(user);
+            // console.log(user);
             table.innerHTML += `<tr>
                             <td>
                                 <input type="text" class="form-control" id="" value="${user.first_name}">
@@ -27,3 +27,51 @@ function getUserList() {
         }
     })
 } getUserList()
+
+function refreshData(){
+   getUserList() 
+}
+
+function createUser(){
+  
+    let data = {
+        first_name:document.querySelector("#first_name").value,
+        last_name:document.querySelector("#last_name").value,
+        email:document.querySelector("#email").value,
+
+    };
+    fetch("https://reqres.in/api/users",{
+        method:"POST",
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        // console.log(data);
+        table.innerHTML += `<tr>
+                            <td>
+                                <input type="text" class="form-control" id="" value="${data.first_name}"/>
+                            </td>
+                            <td>
+                                <input type="text" class="form-control" id="" value="${data.last_name}"/>
+                            </td>
+                            <td>
+                                <input type="text" class="form-control" id="" value="${data.email}"/>
+                            </td>
+                            <td>
+                                <a href="" class="btn btn-warning" onclick="updateUser(${data.id})">Güncelle</a>
+                                <a href="" class="btn btn-danger"  onclick="deleteUser(${data.id})">Sil</a>
+
+                            </td>
+                        </tr>`
+                      
+    })
+    .catch((error) =>{
+        console.log("Hata", error);
+    })
+    
+    
+
+}
